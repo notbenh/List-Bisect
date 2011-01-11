e471ff4479ded69dbf3da48dc26b9d2b68375db2
ben hengst <notbenh@cpan.org>
2011-01-10 16:20:02 -0800

    most basic working example

--- lib/List/Bisect.pm
+++ lib/List/Bisect.pm
@@ -1,5 +1,39 @@
+package List::Bisect;
 use strict;
 use warnings;
-package List::Bisect;
+use Exporter qw{import};
+our @EXPORT = qw{bisect};
+
+# ABSTRACT: split a list in to two parts by way of a grep like block
+
+=head1 EXPORTED FUNCTION
+
+=head2 bisect
+
+  my ($a,$b) = bisect {$_ <= 5} 1..10;
+  # $a == [1..5]
+  # $b == [6..10]
+
+Useage is like grep where you pass it a block and a list, returns a list of two arrayrefs. All 
+TRUE values are put in to the first arrayref, FALSE in the second arrayref.
+
+=cut
+
+sub bisect (&@) {
+   my $grep = shift;
+
+   my @true;
+   my @false;
+   foreach $_ (@_) {
+      if (&$grep) {
+         push @true, $_;
+      }
+      else {
+         push @false, $_;
+      }
+   }
+
+   return (\@true,\@false);
+}
 
 1;
